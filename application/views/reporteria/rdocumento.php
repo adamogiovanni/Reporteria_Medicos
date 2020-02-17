a671e825ee29eca894767a0355f869be80888093
adamogiovanni <adamogiovanni@gmail.com>
2020-02-17 17:06:13 -0300

    Agregar JS Total

--- application/views/reporteria/rdocumento.php
+++ application/views/reporteria/rdocumento.php
@@ -333,6 +333,7 @@ language: {
         last: "<i class='fa fa-fast-forward fa-lg' aria-hidden='true'></i>"
 
       }
+
       //,
       //aria: {
       //    sortAscending: ": activate to sort column ascending",
@@ -404,7 +405,6 @@ var max = 0;
                 "max" : max};
 
 $('#example').dataTable( {
-
 language: {
       processing: "<i class='fa fa-spinner fa-5x fa-spin fa-fw' aria-hidden='true'></i>",
       search: "<i class='fa fa-search' aria-hidden='true'></i>",
@@ -432,6 +432,40 @@ language: {
     data:parametros,
     url:'rdocumento/consultaprovision',
     type: 'POST'
+  },
+
+  "footerCallback": function ( row, data, start, end, display ) 
+  {
+    var api = this.api(), data;
+
+    // Remove the formatting to get integer data for summation
+    var intVal = function ( i ) {
+        return typeof i === 'string' ?
+            i.replace(/[\$,]/g, '')*1 :
+            typeof i === 'number' ?
+                i : 0;
+    };
+
+    // Total over all pages
+    total = api
+        .column( 8 )
+        .data()
+        .reduce( function (a, b) {
+            return intVal(a) + intVal(b);
+        }, 0 );
+
+    // Total over this page
+    pageTotal = api
+        .column( 8, { page: 'current'} )
+        .data()
+        .reduce( function (a, b) {
+            return intVal(a) + intVal(b);
+        }, 0 );
+
+    // Update footer
+    $( api.column( 8 ).footer() ).html(
+        '$'+pageTotal +' ( $'+ total +' total)'
+    );
   },
   dom: 'Bfrtip',
   lengthMenu: [
