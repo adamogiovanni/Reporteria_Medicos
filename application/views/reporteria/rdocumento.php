1f0258b09b52da557742bac10c6979b6c04b0015
Brian Adamo <Brianadamo0403@gmail.com>
2020-02-20 16:11:58 -0300

    2.2 Totales

--- application/views/reporteria/rdocumento.php
+++ application/views/reporteria/rdocumento.php
@@ -380,8 +380,8 @@ $(document).ready(function()
 
       // Update footer
       //$( api.column( 7 ).footer() ).html(('$'+pageTotal));//'$'+ pageTotal +'( $'+ total +' total)');
-      $('tr:eq(0) td:eq(2)', api.table().footer()).html(format(pageTotal, ''));
-      $('tr:eq(1) td:eq(2)', api.table().footer()).html(format(total, ''));
+      $('tr:eq(0) td:eq(2)', api.table().footer()).html(pageTotal);
+      $('tr:eq(1) td:eq(2)', api.table().footer()).html(total);
 
 
     },
