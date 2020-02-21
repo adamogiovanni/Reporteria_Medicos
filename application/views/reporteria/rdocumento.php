623be7c3360da2c0e71997fce15a74a356e01d12
Brian Adamo <Brianadamo0403@gmail.com>
2020-02-21 02:09:29 -0300

    4.5 Totales Resuelto

--- application/views/reporteria/rdocumento.php
+++ application/views/reporteria/rdocumento.php
@@ -564,7 +564,7 @@ $(document).ready(function()
        // Update footer
       //$( api.column( 7 ).footer() ).html(('$'+pageTotal));//'$'+ pageTotal +'( $'+ total +' total)');
 
-      var numFormat = $.fn.dataTable.render.number( '\.', ',', 0,).display;      
+      var numFormat = $.fn.dataTable.render.number( '\,', '.', 0,).display;      
       $('tr:eq(0) th:eq(1)', api.table().footer()).html('$'+ numFormat(pageTotal));
       $('tr:eq(1) th:eq(1)', api.table().footer()).html('$'+ numFormat(total));
 
