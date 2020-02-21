24d07d0e63c4288914684d7e523984e9fb0be37a
Brian Adamo <Brianadamo0403@gmail.com>
2020-02-21 00:54:15 -0300

    3.5 Prueba solucion TOtales

--- application/views/reporteria/rdocumento.php
+++ application/views/reporteria/rdocumento.php
@@ -400,9 +400,11 @@ $('#example').dataTable( {
 
 
       // Update footer
-      //$( api.column( 7 ).footer() ).html(('$'+pageTotal));//'$'+ pageTotal +'( $'+ total +' total)');      
+      //$( api.column( 7 ).footer() ).html(('$'+pageTotal));//'$'+ pageTotal +'( $'+ total +' total)');
+
+      var numFormat = $.fn.dataTable.render.number( '\.', ',', 0,).display;      
       $('tr:eq(0) th:eq(1)', api.table().footer()).html(pageTotal);
-      $('tr:eq(1) th:eq(1)', api.table().footer()).html(total);
+      $('tr:eq(1) th:eq(1)', api.table().footer()).html(numFormat(total));
 
 
     },
